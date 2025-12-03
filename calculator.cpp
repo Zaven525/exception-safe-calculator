675c5eef39bd59ed70dab7b1d891f5ed7118e120
Zaven525 <71457724+Zaven525@users.noreply.github.com>
2025-12-03 19:20:40 +0400

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+
+double plus(double lhs, double rhs)
+{
+    return lhs + rhs;
+}
+
+double minus(double lhs, double rhs)
+{
+    return lhs - rhs;
+}
+int main()
+{
+    double lhs = 0, rhs = 0;
+    std::string operation;
+    std::cin >> lhs;
+    plus(lhs, 2);
+
+}
